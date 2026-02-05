e745cadbc102d9a3a322b7ec325b95896eef2a20
Lavínia <lavizsenai@gmail.com>
2026-02-05 11:13:02 -0300

    fazendo

--- menuObjetos/src/Main.java
+++ menuObjetos/src/Main.java
@@ -1,15 +1,62 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        menu();
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void menu(){
+        System.out.println("==== Secretaria ====");
+        System.out.println("1 - Alunos");
+        System.out.println("2 - Turmas");
+        String opcao = Leitura.dados("Digite a opção desejada: ");
+
+        if (opcao.equals("1")){
+            System.out.println("==== Alunos ====");
+            System.out.println("1 - Listar Alunos");
+            System.out.println("2 - Cadastrar Aluno");
+            System.out.println("3 - Atualizar Aluno");
+            System.out.println("4 - Excluir Aluno");
+            System.out.println("5 - Voltar ao Menu Principal");
+            opcao = Leitura.dados("Digite a opção desejada: ");
+            switch (opcao){
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                default:
+                    System.out.println("Opção inválida, tente novamente");
+            }
+        }
+        else if (opcao.equals("2")){
+            System.out.println("==== Turmas ====");
+            System.out.println("1 - Listar Turmas");
+            System.out.println("2 - Cadastrar Turma");
+            System.out.println("3 - Atualizar Turma");
+            System.out.println("4 - Excluir Turma");
+            System.out.println("5 - Voltar ao Menu Principal");
+            opcao = Leitura.dados("Digite a opção desejada: ");
+            switch (opcao){
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                default:
+                    System.out.println("Opção inválida, tente novamente");
+            }
+        }
+        else {
+            System.out.println("Opção inválida, tente novamente");
         }
     }
 }
