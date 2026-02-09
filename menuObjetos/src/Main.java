e0bae6379f06366c67da821ea805de109bf4be2a
Lavínia <lavizsenai@gmail.com>
2026-02-09 11:05:52 -0300

    continuação

--- menuObjetos/src/Main.java
+++ menuObjetos/src/Main.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 
 public class Main {
@@ -20,21 +19,19 @@ public class Main {
 
         if (opcao.equals("1")) {
             menuAlunos();
-        }
-        else if (opcao.equals("2")) {
+        } else if (opcao.equals("2")) {
             menuTurmas();
-        }
-        else if (opcao.equals("3")) {
+        } else if (opcao.equals("3")) {
             System.out.println("Até breve...");
             System.exit(0);
-        }
-        else {
+        } else {
             System.out.println("Opção inválida! Tente novamente");
+            menuPrincipal();
         }
     }
 
     private static void menuAlunos() {
-        System.out.println("==== Alunos ====");
+        System.out.println("\n==== Alunos ====");
         System.out.println("1 - Listar Alunos");
         System.out.println("2 - Cadastrar Aluno");
         System.out.println("3 - Atualizar Aluno");
@@ -46,26 +43,31 @@ public class Main {
         switch (opcao) {
             case "1":
                 listarAlunos();
+                menuAlunos();
                 break;
             case "2":
                 //cadastrarAluno();
+                menuAlunos();
                 break;
             case "3":
                 //atualizarAluno();
+                menuAlunos();
                 break;
             case "4":
                 //excluirAluno();
+                menuAlunos();
                 break;
             case "5":
                 menuPrincipal();
                 break;
             default:
                 System.out.println("Opção inválida, tente novamente");
+                menuAlunos();
         }
     }
 
     private static void menuTurmas() {
-        System.out.println("==== Turmas ====");
+        System.out.println("\n==== Turmas ====");
         System.out.println("1 - Listar Turmas");
         System.out.println("2 - Cadastrar Turma");
         System.out.println("3 - Atualizar Turma");
@@ -77,25 +79,94 @@ public class Main {
         switch (opcao) {
             case "1":
                 listarTurmas();
+                menuTurmas();
                 break;
             case "2":
-                //cadastrarTurma();
+                cadastrarTurma();
+                menuTurmas();
                 break;
             case "3":
                 //atualizarTurma();
+                menuTurmas();
                 break;
             case "4":
                 //excluirTurma();
+                menuTurmas();
                 break;
             case "5":
                 menuPrincipal();
                 break;
             default:
                 System.out.println("Opção inválida, tente novamente");
+                menuTurmas();
+        }
+    }
+
+    private static void cadastrarTurma() {
+        Periodo periodo = validarPeriodo();
+
+        String curso = Leitura.dados("Digite o curso: ");
+        while (!isCharacter(curso)) {
+            System.out.println("Nome de curso inválido! Não use números ou caracteres especiais");
+            curso = Leitura.dados("Digite o curso: ");
+        }
+
+        String sigla = Leitura.dados("Digite a sigla: ");
+        boolean repetido = true;
+        while (sigla.isBlank() || !repetido) {
+            System.out.println("Sigla inválida");
+            sigla = Leitura.dados("Digite a sigla: ");
+            sigla = sigla.toUpperCase();
+
+            for (Turma t : listaTurmas){
+                if (t.getSigla().equals(sigla)){
+                    System.out.println("Turma já cadastrada");
+                    repetido = true;
+                }
+            }
+
+            repetido = false;
+        }
+
+        Turma turma  = new Turma(curso, sigla, periodo);
+        listaTurmas.add(turma);
+    }
+
+    private static boolean isCharacter(String texto) {
+        String textoSemNumeros = texto.replaceAll("\\d", "");
+        if (!texto.isBlank() && texto.equals(textoSemNumeros)) //isBlanck = trim() e isEmpty()
+            return true;
+        else
+            return false;
+    }
+
+    private static Periodo validarPeriodo() {
+        String opcaoPeriodo = Leitura.dados("""
+                Digite o número do período escolhido:
+                1 - Matutino
+                2 - Vespertino
+                3 - Noturno
+                4 - Integral""");
+        switch (opcaoPeriodo) {
+            case "1":
+                return Periodo.MATUTINO;
+            case "2":
+                return Periodo.VESPERTINO;
+            case "3":
+                return Periodo.NOTURNO;
+            case "4":
+                return Periodo.INTEGRAL;
+            default:
+                System.out.println("Opção inválida, digite novamente");
+                return validarPeriodo();
         }
     }
 
     private static void listarTurmas() {
+        if (listaTurmas.isEmpty()){
+            System.out.println("Não há turmas cadastradas");
+            return;
+        }
         System.out.println("Turmas existentes:");
         for (Turma t : listaTurmas){
             System.out.println(t);
@@ -103,6 +174,10 @@ public class Main {
     }
 
     private static void listarAlunos() {
+        if (listaAlunos.isEmpty()){
+            System.out.println("Não há alunos cadastrados");
+            return;
+        }
         System.out.println("Alunos existentes:");
         for (Aluno a : listaAlunos){
             System.out.println(a);
